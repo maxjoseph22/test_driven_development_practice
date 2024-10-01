9aa9470f7a6d06e2cf6a1e3b6f9b42f5f52f88f6
Max Joseph <maxjoseph22@gmail.com>
2024-10-01 14:22:09 +0100

    testing functions complete

--- tests/test_codeword.py
+++ tests/test_codeword.py
@@ -1,9 +1,20 @@
 from lib.check_codeword import *
 
 def test_codeword():
-    result = check_codeword()
-    assert result ==
+    result = check_codeword("horse")
+    assert result == "Correct! Come in."
 
 def test_codeword_2():
-    result = check_codeword():
-    assert result ==
+    result = check_codeword("house")
+    assert result == "Close, but nope."
+
+def test_codeword_3():
+    result = check_codeword("water")
+    assert result == "WRONG!"
+
+def test_codeword_4():
+    result = check_codeword("hors")
+    assert result == "WRONG!"
+
+    
+                        
